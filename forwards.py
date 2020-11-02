9ae60c5aacb8db28217ffe2db36ea7107b80f03a
JordanWildon <jordanwildon@googlemail.com>
2020-11-02 18:11:30 +0100

    Update forwards.py
    update file save to name

--- forwards.py
+++ forwards.py
@@ -88,7 +88,7 @@ if next1 == 'y':
                         print("An exception occurred")
             print("# # # # # # # # # # Next channel: ", i, "# # # # # # # # # #")
         df = pd.DataFrame(l, columns = ['From', 'To'])
-        df.to_csv("rannet.csv")
+        df.to_csv("net.csv")
 
     with client:
         client.loop.run_until_complete(new_main())
